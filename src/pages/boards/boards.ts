404429491ad75a75ffa837fd81c28de8ae9f6dba
Martin Delfs <Martin Delfs>
2017-02-08 19:11:55 +0100

    got navParam to work

--- src/pages/boards/boards.ts
+++ src/pages/boards/boards.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { Board } from '../../models/board';
+import { HomePage } from '../home/home';
 
 /*
   Generated class for the Boards page.
@@ -18,8 +19,9 @@ export class BoardsPage {
   	this.fillArray();
   }
 
-  public selectedBoard:Board;
   public boards:Array<Board> = [];
+  public selectedBoard:Board;
+  public aBoard = new Board("Sanchi", "Dark grey", 12.4, 699);
   
   fillArray()
   {
@@ -27,8 +29,13 @@ export class BoardsPage {
     	this.boards.push(new Board("Delkra", "redhot", 10.4, 599));
     	this.boards.push(new Board("Sanchi", "Dark grey", 12.4, 699));
 
-    	this.selectedBoard = new Board("", "", 0, 0);
-
+      this.selectedBoard = new Board("Brand", "", 0, 0);  
   }
 
+  buyBoard(board:Board)
+  {
+    this.navCtrl.push(HomePage, board);
+  }
+
+ 
 }
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, NavParams } from 'ionic-angular';
 import { Skateboard } from '../../models/skateboard';
 
 // Import pages
@@ -8,18 +8,29 @@ import { WheelsPage } from '../wheels/wheels';
 import { TrucksPage } from '../trucks/trucks';
 import { BearingsPage } from '../bearings/bearings';
 import { GriptapePage } from '../griptape/griptape';
-
+//TEST
+import { Board } from '../../models/board';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(private navCtrl: NavController) {
+  constructor(private navCtrl: NavController, public navParams: NavParams) {
+    this.myBoard = this.navParams.get('brand');
+    this.myBoardColor = this.navParams.get('color');
+    this.myBoardSize = this.navParams.get('size');
+    this.myBoardPrice = this.navParams.get('price');
+  }  
+  public myBoard: any;
+  public myBoardColor: any;
+  public myBoardSize: any;
+  public myBoardPrice: any;
 
-  }
 
-  Skateboard:Skateboard;
+  skateboard:Skateboard;
+  // selectedBoard:Board = this.navParams.get('id');
+
 
   boardsPage()
   {
